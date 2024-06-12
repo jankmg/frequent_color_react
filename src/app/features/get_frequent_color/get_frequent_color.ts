f2e0fa1a905ec79e827be68b50b833a506d9a7cb
jankmg <jankmgdev@gmail.com>
2024-06-11 19:19:46 -0500

    Add: Multiple color formats

--- src/app/features/get_frequent_color/get_frequent_color.ts
+++ src/app/features/get_frequent_color/get_frequent_color.ts
@@ -10,14 +10,8 @@ const initialState: GetFrequentColorInitialState = {
 
 export const getFrequentColorRequest = createAsyncThunk("frequentColor/frequentColorDataStatus", async(image_url: string)=>{
     try {
-        // const frequentColorResponse: AxiosResponse<ColorRequest> = await axios.get<ColorRequest>(`https://api.jankmg.com/get_dominant_color?image_url=${image_url}`, {
-        // })
-
-        const frequentColorResponse: AxiosResponse<ColorRequest> = await axios.get<ColorRequest>(`http://localhost:8080/get_dominant_color?image_url=${image_url}`, {
+        const frequentColorResponse: AxiosResponse<ColorRequest> = await axios.get<ColorRequest>(`https://api.jankmg.com/get_dominant_color?image_url=${image_url}`, {
         })
-
-        // const frequentColorResponse: AxiosResponse<ColorRequest> = await axios.get<ColorRequest>(`https://jankmgdev.pythonanywhere.com/get_dominant_color?image_url=${image_url}`, {
-        // })
 
         const frequentColorRes: ColorResponse = {
             status: frequentColorResponse.status,
